23a59b7ea8a60e1b0fc41c8e8c7349d631954acb
Harshan <90125014+knucklebar@users.noreply.github.com>
2022-05-26 16:18:38 +0530

    Update leapyear.py

--- leapyear.py
+++ leapyear.py
@@ -1,8 +1,7 @@
-# 🚨 Don't change the code below 👇
-year = int(input("Which year do you want to check? "))
-# 🚨 Don't change the code above 👆
 
-#Write your code below this line 👇
+year = int(input("Which year do you want to check? "))
+
+
 if year%4 == 0:
   if year%100 != 0:
     print("Leap Year")
